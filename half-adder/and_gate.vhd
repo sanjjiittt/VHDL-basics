fa46a10a2874675e80d40db743793691520806fe
sanjjiittt <sanjitprasadwork@gmail.com>
2026-08-18 13:26:20 +0530

    update

--- half-adder/and_gate.vhd
+++ half-adder/and_gate.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08/17/2026 07:45:59 PM
--- Design Name: 
--- Module Name: and_gate - dataflow
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity and_gate is
