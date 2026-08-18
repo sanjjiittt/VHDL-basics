9acfff121bc99f921da708d8fbcd81d584dc2928
sanjjiittt <sanjitprasadwork@gmail.com>
2026-08-18 13:38:09 +0530

    comment removal

--- logic-gates/logic gates.vhd
+++ logic-gates/logic gates.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08/17/2026 01:35:02 PM
--- Design Name: 
--- Module Name: logic gates - Dataflow
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity logic_gates is
     Port ( A : in STD_LOGIC;
