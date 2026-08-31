a4d32230ada58b125ddd83d97a3392fb11c9f987
sanjjiittt <sanjitprasadwork@gmail.com>
2026-08-31 13:03:33 +0530

    syntax update

--- sequential-circuits/D_FF.vhd
+++ sequential-circuits/D_FF.vhd
@@ -3,21 +3,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity D_FF is
     Port ( D : in STD_LOGIC;
-           clk : in STD_LOGIC;
-           Q : out STD_LOGIC;
-           Q_bar : out STD_LOGIC);
+           clk, rst : in STD_LOGIC;
+           Q, Q_bar : out STD_LOGIC);
 end D_FF;
 
 architecture Behavioral of D_FF is
+signal temp: std_logic;
 begin
-    process(clk)
-        variable temp: std_logic;
-    begin
-        if rising_edge(clk) then
-            temp := D;
+    process(clk, rst)
+    begin 
+        if rst = '1' then 
+            temp <= '0';
+        elsif rising_edge(clk) then 
+            temp <= D;
         end if;
         Q <= temp;
         Q_bar <= not(temp);
     end process;
-
 end Behavioral;
